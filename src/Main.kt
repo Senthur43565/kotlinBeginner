26f8908eac93c6c8e4b19bc53466a352f3a291bf
root <selvasp338@gmail.com>
2024-03-09 12:00:49 +0530

    added println

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,4 @@
-import java.lang.ArithmeticException
+//import java.lang.ArithmeticException
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
